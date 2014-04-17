4bb856e2f17d8554f3b7597da3eb98b56b1393dd
Masato Ikeda <masato.ikeda@gmail.com>
2014-04-17 14:41:15 +0900

    こんなかんじ？

--- lib/rake_dump_fixtures.rb
+++ lib/rake_dump_fixtures.rb
@@ -1,5 +1,12 @@
 require "rake_dump_fixtures/version"
+require "rails"
 
 module RakeDumpFixtures
-  # Your code goes here...
+  class Railtie < Rails::Railtie
+    railtie_name :rake_dump_fixtures
+
+    rake_tasks do
+      load "rake_dump_fixtures/dump_fixtures.rake"
+    end
+  end
 end
